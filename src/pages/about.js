805ca359b54e3cdee151eebf7b8646ce891f0ed2
Josh Keller <jkeller@awolff.com>
2021-06-30 13:41:00 -0700

    Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About Me">
+            <p>This is the about me page.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
